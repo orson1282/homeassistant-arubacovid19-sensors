c487252d8de0ae51b14d16d84070e68b76913afb
Orson Oehlers <orson@oehlers.net>
2020-04-12 18:44:09 -0400

    Added support for the active cases.

--- arubacovid19.py
+++ arubacovid19.py
@@ -11,10 +11,12 @@ def getcovid():
     positivo = soup.find(id='comp-k7yvjsdj').span.text
     recupera = soup.find(id='comp-k8bojc5u').span.text
     morto = soup.find(id='comp-k8bojw0s').span.text
+    activo = soup.find(id='comp-k8q4vxzi').span.span.span.span.text
     data = {}
-    data['positivo'] = positivo
-    data['morto'] = morto
-    data['recupera'] = recupera
+    data['positivo'] = int(positivo)
+    data['morto'] = int(morto)
+    data['recupera'] = int(recupera)
+    data['activo'] = int(activo)
     json_data = json.dumps(data)
     print(json_data)
     return;
